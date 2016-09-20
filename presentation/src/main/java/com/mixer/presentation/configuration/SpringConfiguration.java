73af97117fd883ce79897b61c64db5526b5925bd
Amol <Amol@Amol-PC>
2016-09-20 21:36:20 +0530

    changes realted to resource mapping.

--- presentation/src/main/java/com/mixer/presentation/configuration/SpringConfiguration.java
+++ presentation/src/main/java/com/mixer/presentation/configuration/SpringConfiguration.java
@@ -1,18 +1,40 @@
 package com.mixer.presentation.configuration;
 
+/**
+ * 
+ * @author Amol
+ *
+ */
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
+
+
+/*
+ * @EnableWebMvc : <mvc:annotation-driven /> 
+ */
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.mixer.presentation")
-public class SpringConfiguration {
+public class SpringConfiguration extends WebMvcConfigurerAdapter{
 
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations(
+				"WEB-INF/static/");
+		registry.addResourceHandler("/pages/**").addResourceLocations(
+				"WEB-INF/static/html/");
+	}
+	
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -22,5 +44,16 @@ public class SpringConfiguration {
 
 		return viewResolver;
 	}
+	
+	@Bean
+	public UrlBasedViewResolver staticViewResolver() {
+		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+		viewResolver.setOrder(2);
+		viewResolver.setPrefix("/WEB-INF/static/");
+		viewResolver.setSuffix(".html");
+		viewResolver.setViewClass(JstlView.class);
+		return viewResolver;
+	}
+	
 	 
 }
