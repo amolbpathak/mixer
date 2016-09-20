862fedd21e9c7c1e37deb72086c56eb30469b899
Amol <Amol@Amol-PC>
2016-09-20 23:04:45 +0530

    Added code to get token by trusted authentication.

--- presentation/src/main/java/com/mixer/presentation/controller/TableauController.java
+++ presentation/src/main/java/com/mixer/presentation/controller/TableauController.java
@@ -1,6 +1,16 @@
 package com.mixer.presentation.controller;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
@@ -14,6 +24,66 @@ import com.mixer.presentation.tableau.Demo;
 public class TableauController {
 
 
+	
+	@RequestMapping(value="trustedRequest" , method = RequestMethod.GET)
+	public String trustedRequest(Model model) {
+		
+		 final String user = "amolbpathak@gmail.com";
+	        final String wgserver = "10az.online.tableau.com";
+	        final String dst = "views/Book1/Sheet1";
+	        final String params = ":embed=yes&:toolbar=yes";
+	        OutputStreamWriter out = null;
+	        BufferedReader in = null;
+	
+	        StringBuffer data = new StringBuffer();
+            try {
+				data.append(URLEncoder.encode("username", "UTF-8"));
+			
+            data.append("=");
+            data.append(URLEncoder.encode(user, "UTF-8"));
+            data.append("&");
+            data.append(URLEncoder.encode("client_ip", "UTF-8"));
+            data.append("=");
+            data.append(URLEncoder.encode( "Amol-PC", "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            // Send the request
+            
+			try {
+				URL url = new URL("https://" + wgserver + "/trusted");
+		 
+            URLConnection	conn = url.openConnection();
+			
+            conn.setDoOutput(true);
+            
+            out = new OutputStreamWriter(conn.getOutputStream());
+            
+            // Read the response
+            StringBuffer rsp = new StringBuffer();
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ( (line = in.readLine()) != null) {
+                rsp.append(line);
+            }
+            
+            
+            String ticket = rsp.toString();
+	        
+            model.addAttribute("greeting", ticket);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+	        
+	        
+		return "trustedResponse";
+	}
+	
+	
+	
 	
 	@RequestMapping(value="basicEmbed" , method = RequestMethod.GET)
 	public String signIn(Model model) {
